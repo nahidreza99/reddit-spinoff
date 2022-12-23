1f419714e22042960dc64329b87cca669009fd63
wasiffuad <62903407+Wasif-Fuad@users.noreply.github.com>
2022-12-23 16:37:40 +0600

    backend: authentication and post

--- app.py
+++ app.py
@@ -1,32 +1,58 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect,url_for,jsonify
 import requests
-import praw
+import praw ,json
+from variables import   obj
 app = Flask(__name__)
 
-Client_ID = '5pL7d93G5ZW1FDltt3utFA'
-Client_Secret = 'hbBDgd4VVr4VXz1eT-PIjzgaIZjzog'
-User_Agent = 'script:com.example.myredditapp:v1.2.3 (by u/testbot)'
-
-reddit = praw.Reddit(
-    client_id = 'Iyk7s5_atTdbrkKBGjU-Og',
-    client_secret = 'vevcjIFvOdXkVKo51iGukpHdppJ-SQ',
-    user_agent=User_Agent,
-    redirect_uri='http://127.0.0.1:5000/authorize',
-)
+reddit=obj()
 
 
 @app.route("/authorize")
 def authorize():
     code = request.args.get('code')
+    
     authorization_code = reddit.auth.authorize(code)
     user = reddit.user.me()
-    return 'login successful: '+str(user)
+    print(user)
+    print(type(user))
+    if user:
+
+        return redirect(url_for('home'))
+    else:
+        return "unsuccesful"
 
 @app.route("/")
 def home():
+    
     loginURL = reddit.auth.url(scopes=["*"], state="...", duration="permanent")
+    
     return render_template('index.html', **locals())
 
+
+@app.route('/api/search-subreddits', methods=['POST','GET'])
+def search_subreddits():
+  # Get the search query from the request
+  query = request.form['query']
+
+  # Search for subreddits using PRAW
+  subreddits = reddit.subreddits.search(query,limit=10)
+
+  # Create a list of subreddit names
+  subreddit_names = [subreddit.display_name for subreddit in subreddits]
+  print(subreddit_names)
+  # Return the list of subreddit names as a JSON response
+  return  jsonify({'subreddit_name': subreddit_names})
+
+@app.context_processor
+def inject_user():
+    
+    #json_string = json.dumps(reddit_dict)
+    return dict(reddit=reddit)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
---
+++ variables.py
@@ -0,0 +1,19 @@
+import praw
+
+Client_ID = '5pL7d93G5ZW1FDltt3utFA'
+Client_Secret = 'hbBDgd4VVr4VXz1eT-PIjzgaIZjzog'
+User_Agent = 'script:com.example.myredditapp:v1.2.3 (by u/testbot)'
+
+
+
+def obj():
+    reddit = praw.Reddit(
+    client_id = 'Iyk7s5_atTdbrkKBGjU-Og',
+    client_secret = 'vevcjIFvOdXkVKo51iGukpHdppJ-SQ',
+    user_agent=User_Agent,
+    redirect_uri='http://127.0.0.1:5000/authorize',
+)
+
+
+
+    return reddit
